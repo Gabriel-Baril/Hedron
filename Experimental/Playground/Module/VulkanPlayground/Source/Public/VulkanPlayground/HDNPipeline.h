2b378f993a709919f8dd4a20fe70442adfe10cba
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-15 17:28:43 -0500

    Vulkan Learning (EP4), graphics pipeline configuration

--- Experimental/Playground/Module/VulkanPlayground/Source/Public/VulkanPlayground/HDNPipeline.h
+++ Experimental/Playground/Module/VulkanPlayground/Source/Public/VulkanPlayground/HDNPipeline.h
@@ -9,7 +9,18 @@ namespace hdn
 {
 	struct PipelineConfigInfo
 	{
-
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
 	};
 
 	class HDNPipeline
@@ -20,7 +31,7 @@ namespace hdn
 			const std::string& vertFilepath, 
 			const std::string& fragFilepath, 
 			const PipelineConfigInfo& configInfo);
-		virtual ~HDNPipeline() = default;
+		virtual ~HDNPipeline();
 		HDNPipeline(const HDNPipeline&) = delete;
 		void operator=(const HDNPipeline&) = delete;
 		static PipelineConfigInfo DefaultPipelineConfigInfo(uint32 width, uint32 height);
