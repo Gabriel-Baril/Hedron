f4b55c8ec3dd5a8e71b8d431e86439386d346614
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-25 00:42:42 -0500

    Added inih, first iteration of test runner UI

--- module/hdn.code.experimental.playground/vkplayground/src/ideon/ideon_imgui.h
+++ module/hdn.code.experimental.playground/vkplayground/src/ideon/ideon_imgui.h
@@ -1,9 +1,12 @@
 #pragma once
+
 #include "imgui.h"
 #include "pugixml/pugixml.hpp"
+#include "tiny-process-library/process.hpp"
 
 #include <filesystem>
 #include <vector>
+#include <iostream>
 
 namespace hdn
 {
@@ -31,8 +34,6 @@ namespace hdn
 		u32 successes;
 		u32 failures;
 		u32 expectedFailures;
-		// u32 skips;
-		// durationInSeconds
 	};
 
 	struct ExpressionResult
@@ -265,7 +266,6 @@ namespace hdn
 
 			for (pugi::xml_node node = root.first_child(); node; node = node.next_sibling())
 			{
-				HINFO(node.name());
 				if (Str_Equals(node.name(), TEST_CASE_NODE_NAME))
 				{
 					out.testCaseResults.emplace_back(TestCaseResult{});
@@ -282,7 +282,7 @@ namespace hdn
 			}
 		}
 
-		void LoadTestResultFile(const std::string& path)
+		void LoadTestResultFile(const std::string& path, TestResult& testResult)
 		{
 			pugi::xml_document doc; // Already contains the root node (Catch2TestRun)
 			pugi::xml_parse_result result = doc.load_file(path.c_str());
@@ -290,51 +290,265 @@ namespace hdn
 			{
 				return;
 			}
-			TestResult testResult{};
 			ParseRootNode(doc.first_child(), testResult);
-
-
-
 			HINFO("File parsed!");
+		}
+
+		void LoadTestResultFromMemory(const std::string& buffer, TestResult& testResult)
+		{
+			pugi::xml_document doc; // Already contains the root node (Catch2TestRun)
+			pugi::xml_parse_result result = doc.load_buffer(buffer.c_str(), buffer.size());
+			if (!result)
+			{
+				return;
+			}
+			ParseRootNode(doc.first_child(), testResult);
+		}
+
+
+		void DisplayTestNode(const ExpressionResult& expression, ImGuiTreeNodeFlags treeNodeFlags)
+		{
+			ImGui::TableNextRow();
+
+			ImGui::TableNextColumn();
+			ImGui::TreeNodeEx(expression.originalExpression.c_str(), treeNodeFlags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_Bullet | ImGuiTreeNodeFlags_NoTreePushOnOpen);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("Expression");
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", expression.success);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", !expression.success);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%s", expression.filename.c_str());
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", expression.line);
+		}
+
+		void DisplayTestNode(const SectionResult& section, ImGuiTreeNodeFlags treeNodeFlags)
+		{
+			ImGui::TableNextRow();
+
+			ImGui::TableNextColumn();
+			bool open = ImGui::TreeNodeEx(section.name.c_str(), treeNodeFlags);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("Section");
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", section.overallResults.successes);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", section.overallResults.failures);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%s", section.filename.c_str());
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", section.line);
+
+			if (open)
+			{
+				for (const ExpressionResult& expression : section.expressionResults)
+				{
+					DisplayTestNode(expression, treeNodeFlags);
+				}
+				ImGui::TreePop();
+			}
+		}
+
+		void DisplayTestNode(const TestCaseResult& testCase, ImGuiTreeNodeFlags treeNodeFlags)
+		{
+			ImGui::TableNextRow();
+
+			ImGui::TableNextColumn();
+			bool open = ImGui::TreeNodeEx(testCase.name.c_str(), treeNodeFlags);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("Test Case");
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", testCase.overallResult.success);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", !testCase.overallResult.success);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%s", testCase.filename.c_str());
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", testCase.line);
+
+			if (open)
+			{
+				for (const SectionResult& section : testCase.sectionResults)
+				{
+					DisplayTestNode(section, treeNodeFlags);
+				}
+				for (const ExpressionResult& expression : testCase.expressionResults)
+				{
+					DisplayTestNode(expression, treeNodeFlags);
+				}
+				ImGui::TreePop();
+			}
+		}
+
+		void DisplayTestNode(const TestResult& result, ImGuiTreeNodeFlags treeNodeFlags)
+		{
+			ImGui::TableNextRow();
+
+			ImGui::TableNextColumn();
+			bool open = ImGui::TreeNodeEx(result.context.testExecutableName.c_str(), treeNodeFlags);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("Module");
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", result.overallResultsCases.successes);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("%i", result.overallResultsCases.failures);
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("--");
+			ImGui::TableNextColumn();
+			ImGui::TextDisabled("--");
+
+			if (open)
+			{
+				for (const TestCaseResult& testCase : result.testCaseResults)
+				{
+					DisplayTestNode(testCase, treeNodeFlags);
+				}
+				ImGui::TreePop();
+			}
+		}
+
+		void DisplayTestNode(const std::vector<TestResult>& results, ImGuiTreeNodeFlags treeNodeFlags)
+		{
+			for (const TestResult& result : results)
+			{
+				DisplayTestNode(result, treeNodeFlags);
+			}
 		}
 
 		void Draw()
 		{
-			const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
-			const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
-
-			const ImGuiTableFlags flags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_RowBg | ImGuiTableFlags_NoBordersInBody;
-			const ImGuiTreeNodeFlags tree_node_flags = ImGuiTreeNodeFlags_SpanAllColumns;
-
 			ImGui::Begin("IdeonUI");
-			HelpMarker("See \"Columns flags\" section to configure how indentation is applied to individual columns.");
 			if (ImGui::Button("Run All Tests"))
 			{
-				// 1. Gather all the test projects
-				// 2. Compile the list of test projects concurently
-				// 3. Run the test project executable with the right arguments (--success --durations yes --verbosity high --allow-running-no-tests --reporter xml > test_result.xml)
-				// 4. Collect all the generated 'test_result.xml' files
-				// 5. Parse the generated 'test_result.xml' files
-				// 6. Present the parsed result into a UI
+				// 1. Run build_test_projects.py
+				if (!m_RunningTests)
+				{
+					HWARN("build_test_projects.py started...");
+
+					// TODO: Refactor, proper multhreading management
+					m_WaitThread = std::thread([this]() {
+						this->m_RunningTests = true;
+						std::string command = "python D:/_DEV/hedron/module/hdn.tests/build_test_projects.py"; // TODO: Remove hardcoded path and read it from hedron.ini file
+
+						// 2. Compile the list of test projects concurently
+						TinyProcessLib::Process process(
+							command,
+							"D:/_DEV/hedron/module/hdn.tests",
+							[](const char* /*output*/, size_t /*n*/) {
+								// Do nothing for stdout
+							},
+							[](const char* /*error*/, size_t /*n*/) {
+								// Do nothing for stderr
+							}
+						);
+						int exitStatus = process.get_exit_status();
+						HWARN("----------- Finished Running build_test_projects.py -----------");
+
+						// 3. Run the test project executable with the right arguments (--success --durations yes --verbosity high --allow-running-no-tests --reporter xml > test_result.xml)
+						std::string filePath = "D:/_DEV/hedron/module/hdn.tests/test_executable_list.txt"; // TODO: Refactor, Proper file loading library
+						std::ifstream inputFile(filePath);
+						if (!inputFile.is_open())
+						{
+							HERR("Error: Could not open the file '{0}'", filePath.c_str());
+						}
+
+						std::string line;
+						std::vector<std::string> outTestResultPaths;
+						std::vector<std::string> executableCommands;
+						outTestResultPaths.reserve(10);
+						executableCommands.reserve(10);
+						while (std::getline(inputFile, line))
+						{
+							std::filesystem::path executableFile = line;
+							std::string executableName = executableFile.stem().string();
+							std::string outTestResultFilePath = "D:/_DEV/hedron/module/hdn.tests/" + executableName + ".xml";
+							// std::string executableCommand = line + " --success --durations yes --verbosity high --allow-running-no-tests --reporter xml > " + outTestResultFilePath; // TODO: Refactor, proper string format library
+							std::string executableCommand = line + " --success --durations yes --verbosity high --allow-running-no-tests --reporter xml"; // TODO: Refactor, proper string format library
+							executableCommands.push_back(executableCommand);
+							outTestResultPaths.push_back(outTestResultFilePath);
+							HINFO("Executable -> '{0}' '{1}'", line.c_str(), executableCommand.c_str());
+						}
+
+						// TODO: Make this concurrent
+						std::vector<std::string> testResultsStrs;
+						testResultsStrs.reserve(10);
+						for (const auto& testCommand : executableCommands)
+						{
+							HINFO("Running -> '{0}'", testCommand);
+							testResultsStrs.emplace_back(std::string{});
+							std::string& testResultXML = testResultsStrs.back();
+							TinyProcessLib::Process testExecutableProcess(
+								testCommand,
+								"",
+								[&testResultXML](const char* output, size_t n) {
+									// 4. Collect all the test generated outputs
+									testResultXML.append(output, n);
+								},
+								[](const char* error, size_t n) {
+									for (int i = 0;i < n;i++)
+									{
+										std::cout << error[i];
+									}
+									std::cout << "\n";
+								}
+							);
+							int status = process.get_exit_status();
+						}
+
+						HWARN("Tests Results Finished!");
+
+						m_TestResults.clear();
+						for (const auto& testResultsStr : testResultsStrs)
+						{
+							TestResult result;
+							// 5. Parse the generated test output
+							this->LoadTestResultFromMemory(testResultsStr, result);
+							HINFO("RESULT: {0}", result.context.testExecutableName.c_str());
+							m_TestResults.emplace_back(result);
+						}
+						this->m_RunningTests = false;
+						m_RanTestsAtLeastOneTime = true;
+					});
+					m_WaitThread.detach();
+				}
+				else
+				{
+					HWARN("Test already running");
+				}
 			}
 
-
-			if (ImGui::BeginTable("ideon", 3, flags))
+			if (m_RunningTests)
 			{
-				// The first column will use the default _WidthStretch when ScrollX is Off and _WidthFixed when ScrollX is On
-				ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_NoHide);
-				ImGui::TableSetupColumn("Size", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 12.0f);
-				ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 18.0f);
-				ImGui::TableHeadersRow();
+				ImGui::Text("Running Tests...");
+			}
+			else if (!m_RanTestsAtLeastOneTime)
+			{
+				ImGui::Text("Please Run the Tests");
+			}
+			else
+			{
+				HelpMarker("See \"Columns flags\" section to configure how indentation is applied to individual columns.");
 
-				// Simple storage to output a dummy file-system.
-				struct MyTreeNode
+				const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+				const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
+
+				const ImGuiTableFlags flags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_RowBg | ImGuiTableFlags_NoBordersInBody;
+
+				if (ImGui::BeginTable("ideon", 6, flags))
 				{
-					const char* Name;
-					const char* Type;
-					int         Size;
-					int         ChildIdx;
-					int         ChildCount;
+					// The first column will use the default _WidthStretch when ScrollX is Off and _WidthFixed when ScrollX is On
+					ImGui::TableSetupColumn("Module", ImGuiTableColumnFlags_NoHide);
+					ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 12.0f);
+					ImGui::TableSetupColumn("Success", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 12.0f);
+					ImGui::TableSetupColumn("Failed", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 12.0f);
+					ImGui::TableSetupColumn("Filename", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 18.0f);
+					ImGui::TableSetupColumn("Line", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 18.0f);
+					ImGui::TableHeadersRow();
+
+					// Simple storage to output a dummy file-system.
+					/*
 					static void DisplayNode(const MyTreeNode* node, const MyTreeNode* all_nodes)
 					{
 						ImGui::TableNextRow();
@@ -363,28 +577,21 @@ namespace hdn
 							ImGui::TextUnformatted(node->Type);
 						}
 					}
-				};
+					*/
+					const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_SpanAllColumns;
+					DisplayTestNode(m_TestResults, treeNodeFlags);
 
-				static const MyTreeNode nodes[] =
-				{
-					{ "Root",                         "Folder",       -1,       1, 3    }, // 0
-					{ "Music",                        "Folder",       -1,       4, 2    }, // 1
-					{ "Textures",                     "Folder",       -1,       6, 3    }, // 2
-					{ "desktop.ini",                  "System file",  1024,    -1,-1    }, // 3
-					{ "File1_a.wav",                  "Audio file",   123000,  -1,-1    }, // 4
-					{ "File1_b.wav",                  "Audio file",   456000,  -1,-1    }, // 5
-					{ "Image001.png",                 "Image file",   203128,  -1,-1    }, // 6
-					{ "Copy of Image001.png",         "Image file",   203256,  -1,-1    }, // 7
-					{ "Copy of Image001 (Final2).png","Image file",   203512,  -1,-1    }, // 8
-				};
-
-				MyTreeNode::DisplayNode(&nodes[0], nodes);
-
-				ImGui::EndTable();
+					ImGui::EndTable();
+				}
 			}
+
 			ImGui::End();
 		}
 
 	private:
+		std::thread m_WaitThread;
+		std::vector<TestResult> m_TestResults;
+		bool m_RunningTests = false;
+		bool m_RanTestsAtLeastOneTime = false;
 	};
 }
