ccfbfb755eb5974c4905da88c35c9f312185dae0
Gabriel Baril <gabriel-baril2010@hotmail.com>
2025-11-12 09:06:44 -0500

    Started working on module system

--- module/hdn.code.module.srcdb/src/srcdb/module.cpp
+++ module/hdn.code.module.srcdb/src/srcdb/module.cpp
@@ -3,34 +3,96 @@
 #include "strdb.h"
 #include "core/hash.h"
 #include "core/core_filesystem.h"
+#include "core/ini/ini.h"
+#include "core/memory/core_memory.h"
 
 namespace hdn
 {
 	enum class ModuleProvenance
 	{
 		INTERNAL,
-		EXTERNAL
+		EXTERNAL,
+		COUNT,
+		UNKNOWN
 	};
+
+	static constexpr const char* s_ModuleProvenanceNames[underlying(ModuleProvenance::COUNT)] = {
+		"internal",
+		"external"
+	};
+
+	static ModuleProvenance module_provenance_from_str(const char* str)
+	{
+		char lowerBuffer[16];
+		core_strcpy(lowerBuffer, str);
+		str_to_lowercase(lowerBuffer, strlen(lowerBuffer));
+		for (int i = 0; i < underlying(ModuleProvenance::COUNT); i++)
+		{
+			if (str_equals(s_ModuleProvenanceNames[i], lowerBuffer))
+			{
+				return static_cast<ModuleProvenance>(i);
+			}
+		}
+		return ModuleProvenance::UNKNOWN;
+	}
 
 	enum class ModuleKind
 	{
 		CODE,
-		DATA
+		DATA,
+		COUNT,
+		UNKNOWN
 	};
 
-	struct Module
-	{
-		hstring name;
-		hstring author;
-		u64 version;
-		ModuleProvenance provenance;
-		ModuleKind kind;
+	static constexpr const char* s_ModuleKindNames[underlying(ModuleKind::COUNT)] = {
+		"code",
+		"data"
 	};
+
+	static ModuleKind module_kind_from_str(const char* str)
+	{
+		char lowerBuffer[16];
+		core_strcpy(lowerBuffer, str);
+		str_to_lowercase(lowerBuffer, strlen(lowerBuffer));
+		for (int i = 0; i < underlying(ModuleKind::COUNT); i++)
+		{
+			if (str_equals(s_ModuleKindNames[i], lowerBuffer))
+			{
+				return static_cast<ModuleKind>(i);
+			}
+		}
+		return ModuleKind::UNKNOWN;
+	}
 
 	struct HModuleGlob
 	{
-		unordered_map<u64, Module> modules;
+		unordered_map<hmod_t, Module*> modules;
 	} s_ModuleGlob;
+
+	static hmod_t module_get_handle(const char* name)
+	{
+		HashBuilder hb;
+		hb.add_type<Module>();
+		hb.add_string(name);
+		return static_cast<hmod_t>(hb.get());
+	}
+
+	static void parse_module_file(const fspath& moduleFilePath, Module& mod)
+	{
+		INIReader reader(moduleFilePath.string());
+
+		std::string folderName = moduleFilePath.parent_path().filename().string();
+		mod.name = strdb_allocate(folderName.c_str());
+
+		hdn::string author = ini_get_variable(reader, "module", "Author", "");
+		mod.author = strdb_allocate(author.c_str());
+
+		hdn::string source = ini_get_variable(reader, "module", "Source", "");
+		mod.provenance = module_provenance_from_str(source.c_str());
+
+		hdn::string kind = ini_get_variable(reader, "module", "Kind", "");
+		mod.kind = module_kind_from_str(kind.c_str());
+	}
 
 	static void find_modules_in_subfolders(const char* path)
 	{
@@ -53,18 +115,19 @@ namespace hdn
 						auto ext = subEntry.path().extension();
 						if (ext == ".module")
 						{
-							hasModule = true;
+							HINFO("Found module folder: {0}", entry.path().string().c_str());
+							Module module;
+							parse_module_file(subEntry.path(), module);
+
+
 							break;
 						}
 					}
 				}
-
-				if (hasModule)
-				{
-					HINFO("Found module folder: {0}", entry.path().string().c_str());
-				}
 			}
 		}
+
+
 	}
 
 	void module_init(const char* modulePath)
@@ -78,5 +141,20 @@ namespace hdn
 		Module& mod = s_ModuleGlob.modules[moduleHash];
 		mod.name = strdb_allocate(name);
 
+	}
+
+	Module* module_get(const char* name)
+	{
+		return nullptr;
+	}
+
+	Module* module_get(hmod_t mod)
+	{
+		return nullptr;
+	}
+
+	const fspath& module_base_path(hmod_t mod)
+	{
+		// TODO: insert return statement here
 	}
 }
--- module/hdn.code.module.srcdb/src/srcdb/module.h
+++ module/hdn.code.module.srcdb/src/srcdb/module.h
@@ -6,6 +6,23 @@
 
 namespace hdn
 {
+	using hmod_t = u64;
+
+	struct Module
+	{
+		hstring name;
+		hstring author;
+		u64 version;
+		ModuleProvenance provenance;
+		ModuleKind kind;
+		fspath basePath;
+	};
+
 	void module_init(const char* modulePath);
 	void module_load(const char* name);
+
+	Module* module_get(const char* name);
+	Module* module_get(hmod_t mod);
+	const fspath& module_base_path(hmod_t mod);
+
 }
